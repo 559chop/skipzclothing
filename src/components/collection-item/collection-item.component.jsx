ee7ab58cd48e140346d0c1892329d2c6ce5b70a9
Thien Nguyen <Thiennguyenwsb@gmail.com>
2019-12-07 14:08:20 -0800

    update shop data with add items

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,21 +1,35 @@
 import React from 'react'
+import { useDispatch, connect } from 'react-redux'
+import { addItem } from '../../redux/cart/cart.actions'
+import CustomButton from '../custom-button/custom-button.component'
+
 import './collection-item.style.scss'
 
-const CollectionItem = ({ id, name, price, imageUrl }) => {
+const CollectionItem = ({ item, addItem }) => {
+  const handleCartAdd = () => {
+    addItem(item)
+  }
   return (
     <div className='collection-item'>
       <div
         className='image'
         style={{
-          backgroundImage: `url(${imageUrl})`
+          backgroundImage: `url(${item.imageUrl})`
         }}
       />
       <div className='collection-footer'>
-        <span className='name'>{name}</span>
-        <span className='price'>${price}</span>
+        <span className='name'>{item.name}</span>
+        <span className='price'>${item.price}</span>
       </div>
+      <CustomButton onClick={handleCartAdd}>ADD TO CART</CustomButton>
     </div>
   )
 }
 
-export default CollectionItem
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item))
+})
+export default connect(
+  null,
+  mapDispatchToProps
+)(CollectionItem)
